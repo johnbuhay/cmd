98c20d1b114de498455b784a1c545879c6a11ff9
John Buhay <buhaydev@gmail.com>
2023-10-27 16:36:10 -0400

    improve method of storing secret

--- gh-bookmarks/main.go
+++ gh-bookmarks/main.go
@@ -4,39 +4,28 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"os/user"
 
 	"github.com/google/go-github/v38/github"
 	"golang.org/x/oauth2"
 )
 
 func main() {
-	// Get the user's home directory
-	usr, err := user.Current()
+	// Attempt to read the token from the keystore
+	token, err := readGitHubToken()
 	if err != nil {
-		fmt.Printf("Error getting user's home directory: %v\n", err)
-		os.Exit(1)
-	}
-
-	// Define the path to the token file
-	tokenPath := usr.HomeDir + "/.local/.github-token"
-
-	// Attempt to read the token from the file
-	token, err := readGitHubToken(tokenPath)
-	if err != nil {
-		// If the file doesn't exist or there's an error reading it, prompt the user
-		fmt.Println("GitHub access token not found. Please enter your GitHub access token:")
-		_, err := fmt.Scan(&token)
+		fmt.Println(err.Error())
+		// If the secret doesn't exist or there's an error reading it, prompt the user
+		token, err = getGitHubTokenFromUser()
 		if err != nil {
 			fmt.Printf("Error reading user input: %v\n", err)
 			os.Exit(1)
 		}
 
-		// Save the token to the file
-		err = writeGitHubToken(tokenPath, token)
+		// Save the token to the keystore
+		err = writeGitHubToken(token)
 		if err != nil {
-			fmt.Printf("Error writing token to file: %v\n", err)
-			os.Exit(1)
+			fmt.Printf("Error writing token to keystore: %v\n", err)
+			os.Exit(2)
 		}
 	}
 
