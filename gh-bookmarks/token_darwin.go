c474ce1b122226b71a4f46398ba9fdbf9dffa3f6
John Buhay <buhaydev@gmail.com>
2023-10-28 08:20:52 -0400

    add support for macos keychain

---
+++ gh-bookmarks/token_darwin.go
@@ -0,0 +1,50 @@
+//go:build darwin
+
+package main
+
+import (
+	"log"
+
+	"github.com/99designs/keyring"
+)
+
+var ring keyring.Keyring
+
+func init() {
+	var err error
+	ring, err = keyring.Open(keyring.Config{
+		ServiceName:     "github.com",
+		AllowedBackends: []keyring.BackendType{keyring.KeychainBackend},
+		KeychainName:    "login",
+	})
+
+	if err != nil {
+		log.Fatal("Opening keyring failed")
+	}
+}
+
+// storeToken securely stores the GitHub access token using keychain
+func storeToken(token string) error {
+	return ring.Set(keyring.Item{
+		Key:  "token",
+		Data: []byte(token),
+	})
+}
+
+// getToken retrieves the GitHub access token from keychain
+func getToken() ([]byte, error) {
+	// ring, _ := keyring.Open(keyring.Config{
+	// 	ServiceName: "github.com",
+	// })
+
+	i, err := ring.Get("token")
+	if err != nil {
+		return []byte{}, err
+	}
+	return i.Data, nil
+}
+
+// deleteToken removes the GitHub access token using keychain
+func deleteToken() error {
+	return ring.Remove("token")
+}
